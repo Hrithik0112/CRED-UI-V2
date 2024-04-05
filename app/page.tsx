677767a647447cd1d858f63a2fdf23dbf879e179
Hrithik <slumdog4321@gmail.com>
2024-04-05 08:58:34 +0530

    Feat : added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex w-screen min-h-screen flex-col items-center justify-between ">
+      <Navbar />
       <Hero />
     </main>
   );
