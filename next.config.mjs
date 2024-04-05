d5a9a4ca8ccd385f8a93f8d0080d52ea8d8da9bc
Hrithik <slumdog4321@gmail.com>
2024-04-05 08:02:36 +0530

    Feat : Basic Project Initialized

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+import withVideos from "next-videos";
+
+const nextConfig = {
+  /* config options here */
+};
+
+export default {
+  ...nextConfig,
+  ...withVideos(),
+};
