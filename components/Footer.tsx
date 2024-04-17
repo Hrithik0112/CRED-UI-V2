4f576ccdbf3cf22ac1d0401fb9a1f73387a28b37
Hrithik <slumdog4321@gmail.com>
2024-04-17 23:49:50 +0530

    Feat: Added portfolio link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <div className="flex w-screen h-[200px] text-center font-denton text-5xl font-bold justify-center items-center text-white">
-      Made With ❤️ by Hrithik
+      Made With ❤️ by &nbsp; <Link href="https://hrithik-portfolio-one.vercel.app/">Hrithik</Link>
+      
     </div>
   );
 };
